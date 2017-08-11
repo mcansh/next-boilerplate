0c5688c2b0c82f0ca12f62a41ff3bc66c75edbed
Logan McAnsh <logan@mcan.sh>
2017-08-10 22:47:40 -0400

    run child processes

---
+++ utils/exec.js
@@ -0,0 +1,17 @@
+const childProcess = require('child_process');
+
+module.exports.spawn = (command, args) => {
+  return new Promise((resolve, reject) => {
+    const child = childProcess.spawn(command, args, {
+      cwd: process.cwd(),
+      stdio: 'inherit',
+    });
+
+    child.on('exit', () => {
+      resolve();
+    });
+    child.on('error', (err) => {
+      reject(err);
+    });
+  });
+};
