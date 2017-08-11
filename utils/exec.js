c2712cdcaeb43a16532279686b6703e5cf9e8c98
Logan McAnsh <logan@mcan.sh>
2017-08-10 23:30:55 -0400

    eslint error fix

--- utils/exec.js
+++ utils/exec.js
@@ -1,17 +1,15 @@
 const childProcess = require('child_process');
 
-module.exports.spawn = (command, args) => {
-  return new Promise((resolve, reject) => {
-    const child = childProcess.spawn(command, args, {
-      cwd: process.cwd(),
-      stdio: 'inherit',
-    });
-
-    child.on('exit', () => {
-      resolve();
-    });
-    child.on('error', (err) => {
-      reject(err);
-    });
+module.exports.spawn = (command, args) => new Promise((resolve, reject) => {
+  const child = childProcess.spawn(command, args, {
+    cwd: process.cwd(),
+    stdio: 'inherit',
   });
-};
+
+  child.on('exit', () => {
+    resolve();
+  });
+  child.on('error', (err) => {
+    reject(err);
+  });
+});
