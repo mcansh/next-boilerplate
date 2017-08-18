85b4e5360e282a7fb51f5245af0cf40371878bd9
Logan McAnsh <logan@mcan.sh>
2017-08-18 11:46:29 -0400

    Cleanup (#2)
    * import only needed colors from chalk
    
    * start separating into separate functions
    
    * update functions so theres no extra whitespace
    
    * 1.0.2

--- bin/create-nextjs-app.js
+++ bin/create-nextjs-app.js
@@ -1,83 +1,82 @@
 #!/usr/bin/env node
 const fs = require('fs');
-const chalk = require('chalk');
+const { red, green, dim } = require('chalk');
 const path = require('path');
 const spawn = require('../utils/exec').spawn;
 
-function scaffold() {
-  return Promise.resolve()
-    .then(() => {
-      // eslint-disable-next-line global-require, import/no-dynamic-require
-      const packageJSON = require(path.join(process.cwd(), 'package.json'));
-      if (!packageJSON.dependencies) {
-        packageJSON.dependencies = {};
-      }
-      if (!packageJSON.dependencies.next) {
-        packageJSON.dependencies.next = 'latest';
-      }
-      if (!packageJSON.dependencies.react) {
-        packageJSON.dependencies.react = 'latest';
-      }
-      if (!packageJSON.dependencies['react-dom']) {
-        packageJSON.dependencies['react-dom'] = 'latest';
-      }
-      if (!packageJSON.dependencies['prop-types']) {
-        packageJSON.dependencies['prop-types'] = 'latest';
-      }
-      if (!packageJSON.devDependencies) {
-        packageJSON.devDependencies = {};
-      }
-      if (!packageJSON.devDependencies.eslint) {
-        packageJSON.devDependencies.eslint = 'latest';
-      }
-      if (!packageJSON.devDependencies['eslint-config-airbnb']) {
-        packageJSON.devDependencies['eslint-config-airbnb'] = 'latest';
-      }
-      if (!packageJSON.devDependencies['eslint-plugin-import']) {
-        packageJSON.devDependencies['eslint-plugin-import'] = 'latest';
-      }
-      if (!packageJSON.devDependencies['eslint-plugin-jsx-a11y']) {
-        packageJSON.devDependencies['eslint-plugin-jsx-a11y'] = 'latest';
-      }
-      if (!packageJSON.devDependencies['eslint-plugin-react']) {
-        packageJSON.devDependencies['eslint-plugin-react'] = 'latest';
-      }
-      if (!packageJSON.scripts) {
-        packageJSON.scripts = {};
-      }
-      if (!packageJSON.scripts.dev) {
-        packageJSON.scripts.dev = 'next';
-      }
-      if (!packageJSON.scripts.build) {
-        packageJSON.scripts.build = 'next build';
-      }
-      if (!packageJSON.scripts.start) {
-        packageJSON.scripts.publish = 'next start';
-      }
-      fs.writeFileSync('package.json', JSON.stringify(packageJSON, null, '\t'));
-      return packageJSON;
-    })
-    .then((packageJSON) => {
-      console.log(`${chalk.dim('[2/4]')} 🌳  Creating basic architecture...`);
-      fs.mkdirSync(path.join(process.cwd(), 'components'));
-      fs.mkdirSync(path.join(process.cwd(), 'pages'));
-      fs.mkdirSync(path.join(process.cwd(), 'layouts'));
-      const metaComponent = `
+const addDependencies = (packageJSON) => {
+  if (!packageJSON.dependencies) {
+    packageJSON.dependencies = {};
+  }
+  if (!packageJSON.dependencies.next) {
+    packageJSON.dependencies.next = 'latest';
+  }
+  if (!packageJSON.dependencies.react) {
+    packageJSON.dependencies.react = 'latest';
+  }
+  if (!packageJSON.dependencies['react-dom']) {
+    packageJSON.dependencies['react-dom'] = 'latest';
+  }
+  if (!packageJSON.dependencies['prop-types']) {
+    packageJSON.dependencies['prop-types'] = 'latest';
+  }
+};
+
+const addDevDependencies = (packageJSON) => {
+  if (!packageJSON.devDependencies) {
+    packageJSON.devDependencies = {};
+  }
+  if (!packageJSON.devDependencies.eslint) {
+    packageJSON.devDependencies.eslint = 'latest';
+  }
+  if (!packageJSON.devDependencies['eslint-config-airbnb']) {
+    packageJSON.devDependencies['eslint-config-airbnb'] = 'latest';
+  }
+  if (!packageJSON.devDependencies['eslint-plugin-import']) {
+    packageJSON.devDependencies['eslint-plugin-import'] = 'latest';
+  }
+  if (!packageJSON.devDependencies['eslint-plugin-jsx-a11y']) {
+    packageJSON.devDependencies['eslint-plugin-jsx-a11y'] = 'latest';
+  }
+  if (!packageJSON.devDependencies['eslint-plugin-react']) {
+    packageJSON.devDependencies['eslint-plugin-react'] = 'latest';
+  }
+};
+
+const addScripts = (packageJSON) => {
+  if (!packageJSON.scripts) {
+    packageJSON.scripts = {};
+  }
+  if (!packageJSON.scripts.dev) {
+    packageJSON.scripts.dev = 'next';
+  }
+  if (!packageJSON.scripts.build) {
+    packageJSON.scripts.build = 'next build';
+  }
+  if (!packageJSON.scripts.start) {
+    packageJSON.scripts.start = 'next start';
+  }
+};
+
+const metaComponent = packageJSON => (
+  `
 import React from 'react';
 import Head from 'next/head';
 
 const Meta = () => (
-    <Head>
-      <title>${packageJSON.name}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
+  <Head>
+    <title>${packageJSON.name}</title>
+    <meta charSet="utf-8" />
+    <meta name="viewport" content="initial-scale=1.0,   width=device-width" />
+  </Head>
 );
 
 export default Meta;
-      `.trim();
+  `.trim()
+);
 
-      const documentLayout = `
+const documentLayout = (
+  `
 import React from 'react';
 import PropTypes from 'prop-types';
 import Meta from '../components/Meta';
@@ -96,9 +95,11 @@ Document.propTypes = {
 };
 
 export default Document;
-      `.trim();
+  `.trim()
+);
 
-      const indexPgae = `
+const indexPage = packageJSON => (
+  `
 import React from 'react';
 import Document from '../layouts/Document';
 
@@ -109,22 +110,41 @@ const Index = () => (
 );
 
 export default Index;
-      `.trim();
-      fs.writeFileSync(path.join(process.cwd(), 'layouts', 'Document.js'), documentLayout);
-      fs.writeFileSync(path.join(process.cwd(), 'pages', 'index.js'), indexPgae);
-      fs.writeFileSync(path.join(process.cwd(), 'components', 'Meta.js'), metaComponent);
-      return packageJSON;
-    })
-    .then(() => {
-      console.log(`${chalk.dim('[3/4]')} 📜  Creating default .gitignore...`);
-      const gitignore = './.gitignore';
-      if (!fs.existsSync(gitignore)) {
-        const DEFAULT_GITIGNORE = `
+  `.trim()
+);
+
+const DEFAULT_GITIGNORE = `
 node_modules
 *.log
 .DS_Store
 .next
-        `.trim();
+`.trim();
+
+function scaffold() {
+  return Promise.resolve()
+    .then(() => {
+      // eslint-disable-next-line global-require, import/no-dynamic-require
+      const packageJSON = require(path.join(process.cwd(), 'package.json'));
+      addDependencies(packageJSON);
+      addDevDependencies(packageJSON);
+      addScripts(packageJSON);
+      fs.writeFileSync('package.json', JSON.stringify(packageJSON, null, '\t'));
+      return packageJSON;
+    })
+    .then((packageJSON) => {
+      console.log(`${dim('[2/4]')} 🌳  Creating basic architecture...`);
+      fs.mkdirSync(path.join(process.cwd(), 'components'));
+      fs.mkdirSync(path.join(process.cwd(), 'layouts'));
+      fs.mkdirSync(path.join(process.cwd(), 'pages'));
+      fs.writeFileSync(path.join(process.cwd(), 'components', 'Meta.js'), metaComponent(packageJSON));
+      fs.writeFileSync(path.join(process.cwd(), 'layouts', 'Document.js'), documentLayout);
+      fs.writeFileSync(path.join(process.cwd(), 'pages', 'index.js'), indexPage(packageJSON));
+      return packageJSON;
+    })
+    .then(() => {
+      console.log(`${dim('[3/4]')} 📜  Creating default .gitignore...`);
+      const gitignore = './.gitignore';
+      if (!fs.existsSync(gitignore)) {
         fs.writeFileSync(gitignore, DEFAULT_GITIGNORE);
       }
     });
@@ -132,20 +152,20 @@ node_modules
 
 Promise.resolve()
   .then(() => {
-    console.log(`${chalk.dim('[1/4]')} 📦  Creating package.json...`);
+    console.log(`${dim('[1/4]')} 📦  Creating package.json...`);
     return spawn('yarn', ['init']);
   })
   .then(scaffold)
   .then(() => {
-    console.log(`${chalk.dim('[4/4]')} 📦  Installing packages...`);
+    console.log(`${dim('[4/4]')} 📦  Installing packages...`);
     return spawn('yarn', ['install']);
   })
   .then(() => {
-    console.log(`${chalk.green('success 🎉 ')} App initialized`);
+    console.log(`${green('success 🎉 ')} App initialized`);
     process.exit(0);
   })
   .catch((err) => {
-    console.log(`${chalk.red('error ⛔ ')} Error while initializing App`);
+    console.log(`${red('error ⛔ ')} Error while initializing App`);
     console.log((err || {}).body || err);
     process.exit(1);
   });
