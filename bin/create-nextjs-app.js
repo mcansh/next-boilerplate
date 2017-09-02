e1e4a8c094e064c16a62ed5be698e5fc4df1a914
Logan McAnsh <logan@mcan.sh>
2017-09-02 14:58:49 -0400

    indent meta component

--- bin/create-nextjs-app.js
+++ bin/create-nextjs-app.js
@@ -84,11 +84,11 @@ import React from 'react';
 import Head from 'next/head';
 
 const Meta = () => (
-<Head>
-  <title>${pkg.name}</title>
-  <meta charSet="utf-8" />
-  <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-</Head>
+  <Head>
+    <title>${pkg.name}</title>
+    <meta charSet="utf-8" />
+    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+  </Head>
 );
 
 export default Meta;
