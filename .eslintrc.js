e18dee019200dbbafff78e10cde3ea9f35891c9d
Logan McAnsh <logan@mcan.sh>
2017-11-09 09:08:33 -0500

    add additional eslint plugins
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,14 @@
 module.exports = {
-  "extends": "airbnb-base",
+  extends: ['airbnb', 'prettier'],
+  env: {
+    browser: true,
+  },
+  parser: 'babel-eslint',
+  plugins: ['react', 'jsx-a11y', 'import', 'prettier'],
   rules: {
-    'no-console': 0,
-    'no-param-reassign': 0,
-    'prefer-destructuring': 1,
-    'function-paren-newline': 0,
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'jsx-a11y/href-no-hash': 0,
+    'react/jsx-closing-tag-location': 0,
+    'react/jsx-curly-brace-presence': 0,
   },
 };
