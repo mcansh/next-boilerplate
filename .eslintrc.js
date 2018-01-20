6423fad380b92599a20d1755f8d701e271f3997d
Logan McAnsh <logan@mcan.sh>
2018-01-20 18:13:33 -0500

    update direct deps
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,11 @@
 module.exports = {
   extends: ['airbnb', 'prettier'],
   env: {
-    browser: true
+    browser: true,
   },
   parser: 'babel-eslint',
-  plugins: ['react', 'jsx-a11y', 'import', 'prettier'],
+  plugins: ['import', 'prettier'],
   rules: {
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    'jsx-a11y/href-no-hash': 0,
-    'react/jsx-closing-tag-location': 0,
-    'react/jsx-curly-brace-presence': 0,
-    'comma-dangle': [2, 'always-multiline']
-  }
+    'comma-dangle': [2, 'always-multiline'],
+  },
 };
