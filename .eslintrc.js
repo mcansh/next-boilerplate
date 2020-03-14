bd3d3de0204127ae6b25194c23e8c3ab7f289b61
Logan McAnsh <logan@mcan.sh>
2020-03-14 14:00:41 -0400

    chore: update lint-staged config
    adds support for next.js dynamic routes
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
           'next.config.js',
           'prettier.config.js',
           'stylelint.config.js',
+          'lint-staged.config.js',
         ],
       },
     ],
---
+++ lint-staged.config.js
@@ -0,0 +1,24 @@
+const { quote: escape } = require('shell-quote');
+
+const isWin = process.platform === 'win32';
+
+module.exports = {
+  '**/*.{js|jsx|ts|tsx}': filenames => {
+    const escapedFileNames = filenames
+      .map(filename => `"${isWin ? filename : escape([filename])}"`)
+      .join(' ');
+    const filenamesForESLint = filenames.map(f => `"${f}"`).join(' ');
+
+    return [
+      `eslint --fix ${filenamesForESLint}`,
+      `prettier --write ${escapedFileNames}`,
+      `jest --bail --findRelatedTests ${escapedFileNames}`,
+    ];
+  },
+  '**/*.{json|yml|yaml|css|less|scss|md|graphql|mdx}': filenames => {
+    const escapedFileNames = filenames
+      .map(filename => `"${isWin ? filename : escape([filename])}"`)
+      .join(' ');
+    return [`prettier --write ${escapedFileNames}`];
+  },
+};
