8995096669025497abb24a2b9d0b2e07fe018c13
Logan McAnsh <logan@mcan.sh>
2020-03-14 14:04:29 -0400

    chore: remove re-exporting jest-dom's types, add extended process.env type
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

---
+++ @types/global.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="node" />
+
+// Extend the NodeJS namespace with variables in next.config.js
+declare namespace NodeJS {
+  interface ProcessEnv {
+    readonly VERSION: string;
+  }
+}
